5bab4cc28a8171f996ba5128a6b25778f4854614
gregorylull <gregorylull@gmail.com>
2014-01-16 14:45:16 -0800

    planning pseudocode, simple test cases

--- src/mytests.js
+++ src/mytests.js
@@ -1,2 +1,33 @@
 // these are my own tests
 
+var test0 = ['a'];
+var test1 = ['a', 'b'];
+var test2 = [0, 1];
+
+function string1 (arrObjPrim, index, sstr) {
+
+    sstr = sstr || '';
+    
+    if (typeof arrObjPrim === "string" || typeof arrObjPrim === "number") {
+	console.log(arrObjPrim);
+	return arrObjPrim;
+
+    } else if ( Array.isArray(arrObjPrim)) {
+	console.log(arrObjPrim);
+        return sstr + '[' + string1(arrObjPrim[index], sstr) + ']';
+    }
+}
+
+string1(test2, 0);
+
+var string2 = function (elt, array, result, index) {
+  result = result || '';
+
+  if (Array.isArray(elt)) {
+      return result + '[' + string2(elt[0], elt.slice(1), ) + ']';
+  } else {
+      return elt;
+  }
+};
+
+console.log(string2(test0));
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,63 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
+// planning
+
+/*******************************************************************************
+
+  // termination cases
+
+  // base cases
+
+  // recursion cases
+
+  1. call function on every element in an array to determine if: prim, obj, or arr
+  2. when calling function on an array, add brackets '[' + func(primObjArr) + ']';
+
+
+if (objArrPrim === 'array') {
+  for (var i = 0; i < length; i++) {
+    
+  }  
+}
+
+-----
+
+for a simple array ['a', 'b']
+
+can create an empty parent array that holds values [index0, index1]
+
+index0 = stringified 'a', index1 = stringified 'b'
+
+base cases: 
+  if string -> add string to result string  // result + ', ' + string;
+  if number -> add number to result string  // result + ', ' + number;
+  if array -> add to result string with brackets "[" + Func(arr) + "]", Func: create parent string (''), iterate through, call function on each element
+  if object -> add to result string with braces "{" 
+
+---
+
+function stringify (arrPrimObj) {
+  
+  if (typeof arrPrimObj 
+
+}
+
+*******************************************************************************/
+
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // starting
   
 };
+
+
+// my own test cases
+var test0 = ['a'];
+var test1 = ['a', 'b', 'c'];
+var test2 = ['a', 'b', 2, 'c'];
+var test3 = ['a', 'b', ['c']];
+var test4 = ['a', 'b', [2, 'c']];
+var test5 = ['a', 'b', [[2], 'c']];
+var test6 = ['a', {no: 'yes'}, 'b'];
+var test7 = ['a', {no: 'yes'}, 'b', ['c', {d: ['three', 3, '3']}]];
