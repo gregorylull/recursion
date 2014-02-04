208faf18915bc1d580338c0000cc079d73203e26
gregorylull <gregorylull@gmail.com>
2014-02-03 17:38:39 -0800

    understanding different methods of the document node system

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -11,4 +11,38 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var targetArray = [];  
+
+  var getElements = function (parentNode) {
+      if (parentNode.classList('target')) {
+	  targetArray.push(parentNode);
+      }
+
+      if (parentNode.firstChild === null) {
+	  getElements(parentNode.nextSibling);
+      } else {
+	  
+      }
+  };
+      
 };
+
+
+var allNodes = document.body.childNodes;
+
+// note if no classList, then undefined. 
+
+/*
+<body>
+  <p> has child </p>
+</body>
+
+note:  document.body.childNodes[0].hasChildNodes === true 
+
+  this is because 'has child' is a TEXT node!!
+
+*/
+
+for (var i = 0, length = allNodes.length; i < length; i++ ) {
+    console.log(allNodes.classList.contains('target'));
+}
