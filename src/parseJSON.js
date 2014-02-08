1579a775ed401146b804a83cfcc62c738f337da1
gregorylull <gregorylull@gmail.com>
2014-02-08 17:43:09 -0500

    added hasContainerFunc
    current plan: iterate through json string, when char === { || [ return that object then call function on the substring..?

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -19,16 +19,64 @@
   4. assuming valid JSON, always will have end bracket, braches, and double quotes.
   5. helper function: 
      - find index of other end of quote?
-       findEnd(
+       case: [5]
+             [3,[5]]
+             [{yes, "no"}, "no", [[3]]] 
+             [ [ [ 3 ] ] ]
+     
+       findEnd(doubleQuote / brace / bracket) {
+           var withDoubleQuotes = false;      // if encounter double quote, set true
+           
+       }
   6. values
      - if string === 'true' 'false'   // NOT double quote
      - if string === 0 - 9        
      - if string === undefined        // NOT in valid JSON
      - if string === null
 
+  OR
+
+  Create an array
+
 *******************************************************************************/
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+    // currentIndex
+    var currentIndex = 0;
+    
+    // helperfunctions
+    
+    // returns true if array or object container has opening and ending
+    var hasContainerEnd = function (openBraceOrBracket, currentIndex) {
+	var closedBraceOrBracket = (openBraceOrBracket === "[" ? "]" : "}");
+	var currentChar = openBraceOrBracket;
+	var withinDoubleQuotes = false;
+	var containerOpened = 0, containerClosed = 0;
+
+        do {
+	    if (!withinDoubleQuotes) {
+		if (currentChar === openBraceOrBracket) {
+		    containerOpened++;
+		} else if (currentChar === closedBraceOrBracket) {
+		    containerClosed++;
+		}
+	    } else if (currentChar === '"') {
+		withinDoubleQuotes = !withinDoubleQuotes;
+	    }
+	    
+	    currentChar = json[++currentIndex];
+	    
+	} while (currentChar && containerOpened !== containerClosed);
+
+	return containerOpened === containerClosed;
+    };
+
+    // [ [], [], [] ];
+    var recursiveFunc (jsonChar, currentIndex, containerBegnningIndex, endOfContainerIndex) {
+    // if beginng of array, check for an end, returns array and its contens
+        if (jsonChar === "[" && hasContainerEnd(jsonChar, currentIndex)) {
+            return [ recursiveFunc(json.substring(currentIndex + 1, ), currentIndex + 1 ];
+        }
+    };
 };
